958207a75fbaea91258e420955bfcd4b2b00176c
konopkagrzeg <konopkagrzeg@gmail.com>
2020-12-21 22:15:21 +0100

    solved exercise 4 & 5

--- part01-e04_multiplication_table/src/multiplication_table.py
+++ part01-e04_multiplication_table/src/multiplication_table.py
@@ -3,6 +3,11 @@
 
 def main():
     pass
+    for i in range(1,11):
+        for j in range(1,11):
+            print('{:4d}'.format(i*j),end="")
+        print()
+
 
 if __name__ == "__main__":
     main()
