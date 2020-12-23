352b78847866ace72b0bbf2102c7ee00aba00d95
konopkagrzeg <konopkagrzeg@gmail.com>
2020-12-23 23:10:51 +0100

    added 9 exercise of part 01

--- part01-e09_merge/src/merge.py
+++ part01-e09_merge/src/merge.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    L = L1 + L2
+    L.sort()
+    return L
 
 def main():
-    pass
-
+    L1 = [1, 7, 8]
+    L2 = [4, 10, 11]
+    print(merge(L1,L2))
+    
 if __name__ == "__main__":
     main()
