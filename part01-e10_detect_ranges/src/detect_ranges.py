f0b81b8c324f2010bc9dc8a2eb6eaae9a5515537
konopkagrzeg <konopkagrzeg@gmail.com>
2020-12-28 22:32:49 +0100

    solved 10 & 11

--- part01-e10_detect_ranges/src/detect_ranges.py
+++ part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    return []
+    # L = sorted(L)
+    # ListRange = []
+    # startValue = 0
+    # endValue = 0
+
+    # for i in range (1,len(L)):
+    #     if (L[i] - 1 == L[i-1])
+    #         endValue = L[i]
+
+    l = sorted(L)
+    l2 = []
+    i = 0
+    while l:
+        if len(l)==1:
+            l2.append(l[0])
+            l.pop(0)
+        elif l[i+1] == l[i] + 1:
+            i += 1
+            if i == len(l)-1:
+                temp = (l[0], l[i]+1)
+                l = []
+                l2.append(temp)
+        else:
+            if i==0:
+                l2.append(l[0])
+                l.pop(0)
+            else:
+                temp = (l[0], l[i]+1)
+                del l[:i+1]
+                l2.append(temp)
+                i = 0
+                return l2
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
